684d10919684b86ad0b13110ecf1f1ad99398780
ctr-code <codeinstitute@charlesreilly.com>
2026-09-14 19:28:07 +0100

    Format the opening hours

--- core/views.py
+++ core/views.py
@@ -228,7 +228,7 @@ def opening_hours(request):
         return {
             # Note that '%-d' is glibc only, i.e. not windows
             "date": f"{day["date"]:%a %-d %b}",
-            "ranges": day["times"] if day["open"] else ["CLOSED"],
+            "ranges": day["times"] if day["open"] else [""],
         }
 
     days = get_opening_hours(date.today(), OPENING_HOURS_DAY_COUNT)
