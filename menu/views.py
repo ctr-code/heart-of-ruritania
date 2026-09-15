c9e151a73e3864255a48fe95cb8003f44e227b6f
ctr-code <codeinstitute@charlesreilly.com>
2026-09-15 20:26:42 +0100

    Render the menu from the model

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Course
 
 
 def menu(request):
     """View for the menu page"""
+
+    courses = Course.objects.all()
+
     return render(
         request,
         "menu/menu.html",
+        {
+            "courses": courses,
+        },
     )
